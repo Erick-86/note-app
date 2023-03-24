dab15bf4bb77ac955365ccf83c586272703b7ed1
FreeMan <zelonaire@gmail.com>
2023-03-24 08:22:54 +0000

    assigning the elements to a variable

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 "use strict";
+
+//Btn to add new note
+const addNoteBtn = document.getElementById("add-note-btn");
+
+//Note forms modal
+const noteForms = document.getElementById("note--form");
+
+//Header counter
+const headerCounter = document.getElementById("note--form");
+
+//Note header input
+const noteHeader = document.getElementById("note-header");
+
+//Note Characters counts
+const noteCharsCount = document.getElementById("note-chars-count");
+
+//Note textarea
+const noteTextarea = document.getElementById("note-textarea");
+
+//Save and Delete btns
+const saveBtn = document.getElementById("save-btn"),
+      delBtn = document.getElementById("del-btn");
